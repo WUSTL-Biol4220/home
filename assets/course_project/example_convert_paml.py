9e261d7369cf674d54524a9b4631c0dc7a27056b
Michael Landis <mlandis@gmail.com>
2020-12-10 15:31:42 -0600

    edit script to mask stop codons

--- assets/course_project/example_convert_paml.py
+++ assets/course_project/example_convert_paml.py
@@ -1,9 +1,13 @@
 from Bio import SeqIO
-
+from Bio.Data import CodonTable
+# get codon table
+standard_table = CodonTable.unambiguous_dna_by_name["Standard"]
+# get stop codons
+stop = standard_table.stop_codons
 # get input filname
 infn = 'all_E.fasta'
 # get output filename
-outfn = 'all_E.phy'
+outfn = 'all_E.paml'
 # create dictionary of sequences from input
 d = SeqIO.to_dict(SeqIO.parse(infn,'fasta'))
 # get number of sequences
@@ -14,7 +18,25 @@ n_sites = len( d[next(iter(d))] )
 s = str(n_seq) + ' ' + str(n_sites) + '\n'
 # print phylip sequence entries
 for k,v in d.items():
-    s += k[:30] + '  ' + str(v.seq) + '\n'
+    # empty sequence content to populat
+    seq = ''
+    # get codons
+    c1 = str(v.seq[0::3])
+    c2 = str(v.seq[1::3])
+    c3 = str(v.seq[2::3])
+    # get num codons
+    n_codons = min([len(c1), len(c2), len(c3)])
+    # loop over codons
+    for i in range(n_codons):
+        # build each codon
+        c123 = c1[i]+c2[i]+c3[i]
+        # mask stop codons
+        if c123 in stop:
+            c123 = 'NNN'
+        # concatenate codons into seq
+        seq += c123
+    # save sequence id + content
+    s += k[:30] + '  ' + seq + '\n'
 
 # open output file
 outf = open(outfn, 'w')
