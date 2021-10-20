d5c945c6317f726c61c23152b0c2798566292cc9
Michael Landis <mlandis@gmail.com>
2021-10-20 10:07:00 -0500

    add match_me script

---
+++ assets/code/match_me.py
@@ -0,0 +1,10 @@
+import random
+
+names = [ 'Hannah Chay', 'Teddy Fujimoto', 'Enrique Gomez Gomez', 'Akshay Govindan', 'Chris Hemauer', 'Caroline Henry', 'Chase Hubbart', 'Nana Kusi', 'Nistha Panda', 'Kenneth Peng', 'Alexandra Sacco', 'Russell Scharf', 'Charles Shen', 'Gary Twu' ]
+
+random.shuffle( names )
+
+n_names = len(names)
+
+print('Go meet ' + names[0] + '!')
+
